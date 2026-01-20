a3283884dc0cb5afca7ae8df3745767f8509921d
devchetan4757 <devchetanpatil@gmail.com>
2026-01-20 12:00:26 +0530

    update

--- frontend/src/components/InstagramBubble.jsx
+++ frontend/src/components/InstagramBubble.jsx
@@ -2,52 +2,46 @@ import { useState } from "react";
 import ReelPlayer from "./ReelPlayer";
 
 const InstagramBubble = ({ url }) => {
-  const [key, setKey] = useState(0); // changing key will reload embed
+  const [replayKey, setReplayKey] = useState(0);
 
   const handleReplay = () => {
-    setKey((prev) => prev + 1); // triggers re-render of ReelPlayer
+    setReplayKey((prev) => prev + 1); // triggers re-mount
   };
 
   return (
     <div
       className="instagram-bubble"
       style={{
-        width: "250px",       // bubble width
-        height: "380px",      // bubble height
+        width: "250px",
+        height: "380px",
         borderRadius: "12px",
-        overflow: "hidden",   // crop the inner iframe
+        overflow: "hidden",
         margin: "5px 0",
         position: "relative",
         backgroundColor: "#000",
       }}
     >
-      {/* Inner wrapper to crop and shift */}
       <div
         style={{
           position: "absolute",
-          top: "-58px",        // crops top black space
+          top: "-58px",
           left: "-39px",
-          width: "300px",      // must be larger than container width
-          height: "530px",     // larger than container height
+          width: "300px",
+          height: "530px",
         }}
       >
-        <ReelPlayer key={key} url={url} />
+        <ReelPlayer key={replayKey} url={url} />
       </div>
 
-      {/* Replay Button */}
       <button
         onClick={handleReplay}
         style={{
           position: "absolute",
           bottom: "5px",
           right: "5px",
-          backgroundColor: "rgba(0,0,0,0.5)",
-          color: "#fff",
-          border: "none",
-          borderRadius: "6px",
-          padding: "2px 6px",
+          zIndex: 10,
+          padding: "4px 8px",
           fontSize: "12px",
-          cursor: "pointer",
         }}
       >
         Replay
