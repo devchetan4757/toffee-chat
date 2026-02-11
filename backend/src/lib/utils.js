303e7ca9ea7f16d3eb0c8f22e8480513e6c7bda8
devchetan4757 <devchetanpatil@gmail.com>
2026-02-11 20:47:02 +0530

    Token age increased

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,11 +5,11 @@ export const generateToken = (res) => {
     { sub: "shared-access", 
       scope : "authorized",
     }, process.env.JWT_SECRET, {
-    expiresIn: "30min",
+    expiresIn: "90min",
   });
 
   res.cookie("jwt", token, {
-    maxAge: 30 * 60 * 1000, // MS
+    maxAge: 90 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development",
