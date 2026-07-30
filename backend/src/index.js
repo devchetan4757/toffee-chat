f9b7dacb696134427a52ec15490c9ed825fc31a0
devchetan4757 <devchetanpatil@gmail.com>
2026-07-30 20:57:34 +0530

    dist

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,10 +41,10 @@ app.use("/api/music", musicRoutes);
 
 // Production build serving
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
